704cefc964c80bc1cb6160760e566da39edadca8
LuigiTroiano <troiano.luigi8@gmail.com>
2021-01-19 16:37:59 +0100

    Menu Jumbotron completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ let app = new Vue({
     opening: "Mon - Sat - 9:00-18:00",
     phoneNumb: "+1 (305) 1234-5678",
     email: "hello@example.com",
-
+    menuJumbotron: ["HOME","ABOUT","SERVICES","PROCESS","TESTIMONIALS"],
   },
   methods: {
 
