5b52ab6010f47a1621cc0280d290d1f16f255bdb
LuigiTroiano <troiano.luigi8@gmail.com>
2021-01-20 11:39:04 +0100

    Corretto errori jumbo/hero
    Ho corretto Vuejs e un po' di nomenclatura css

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,28 @@ let app = new Vue({
     opening: "Mon - Sat - 9:00-18:00",
     phoneNumb: "+1 (305) 1234-5678",
     email: "hello@example.com",
-    menuJumbotron: ["HOME","ABOUT","SERVICES","PROCESS","TESTIMONIALS"],
+    menuJumbotron: [
+      {
+        name: "HOME",
+        url: "#",
+      },
+      {
+        name: "ABOUT",
+        url: "#",
+      },
+      {
+        name: "SERVICES",
+        url: "#",
+      },
+      {
+        name: "PROCESS",
+        url: "#",
+      },
+      {
+        name: "TESTIMONIALS",
+        url: "#",
+      },
+    ]
   },
   methods: {
 
