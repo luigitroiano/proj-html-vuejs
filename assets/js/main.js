63c7aa0fcd70caff831b20fa8f9125b18fd4f3e5
LuigiTroiano <troiano.luigi8@gmail.com>
2021-01-20 13:06:59 +0100

    Footer quasi completato
    Devo solo sistemare il CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ let app = new Vue({
     opening: "Mon - Sat - 9:00-18:00",
     phoneNumb: "+1 (305) 1234-5678",
     email: "hello@example.com",
+    address: "Main Avenue, 987",
     menuJumbotron: [
       {
         name: "HOME",
@@ -25,7 +26,90 @@ let app = new Vue({
         name: "TESTIMONIALS",
         url: "#",
       },
-    ]
+    ],
+    footerLink: [
+      {
+        label: "About",
+        downMenu: [{
+          name: "The Company",
+          url: "#",
+        },
+        {
+          name: "Institutional",
+          url: "#",
+        },
+        {
+          name: "Social & Events",
+          url: "#",
+        },
+        {
+          name: "Innovation",
+          url: "#",
+        },
+        {
+          name: "Environment",
+          url: "#",
+        },
+        {
+          name: "Technology",
+          url: "#"
+        }],
+      },
+      {
+        label: "Transport",
+        downMenu: [{
+          name: "Industrialized",
+          url: "#",
+        },
+        {
+          name: "Chemicals",
+          url: "#",
+        },
+        {
+          name: "Packaged & Liquids",
+          url: "#",
+        },
+        {
+          name: "Construction",
+          url: "#",
+        },
+        {
+          name: "Laminated Wood",
+          url: "#",
+        },
+        {
+          name: "And others",
+          url: "#",
+        }],
+      },
+      {
+        label: "Support",
+        downMenu: [{
+          name: "Responsibility",
+          url: "#",
+        },
+        {
+          name: "Terms of Use",
+          url: "#",
+        },
+        {
+          name: "About Cookies",
+          url: "#",
+        },
+        {
+          name: "Privacy Policy",
+          url: "#",
+        },
+        {
+          name: "Accessibility",
+          url: "#",
+        },
+        {
+          name: "Information",
+          url: "#",
+        }],
+      },
+    ],
   },
   methods: {
 
